fae75af99cde94af4bcc294ad8bdc4ea71371358
David Papp <pappdav@gmail.com>
2025-05-30 20:01:49 +0200

    feat: add NSQ lookup functionality

---
+++ lib/resty/nsq/lookup.lua
@@ -0,0 +1,35 @@
+local h = require("resty.http")
+local cjson = require("cjson")
+local _M = {}
+
+function _M.lookup(url, topic)
+    if not url then
+        return nil, "url is required"
+    end
+    if not topic then
+        return nil, "topic is required"
+    end
+    local httpc = h.new()
+    local res, err = httpc:request_uri(url .. "/lookup?topic=" .. topic, {
+        method = "GET",
+    })
+    if err then
+        return nil, err
+    end
+    if res.status ~= 200 then
+        return nil, "lookup request failed with status: " .. res.status
+    end
+    local body = cjson.decode(res.body)
+    local addresses = {}
+    for _, producer in ipairs(body.producers) do
+        local address = ngx.re.sub(producer.remote_address, ":[0-9]+", "")
+        table.insert(addresses, {
+            host = address,
+            ip = address,
+            port = producer.tcp_port,
+        })
+    end
+    return addresses
+end
+
+return _M
